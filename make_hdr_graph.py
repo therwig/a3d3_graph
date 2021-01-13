af46e732110a9ae62dd49fad5e2b7967ae9dc341
Javier Duarte <jduarte@ucsd.edu>
2021-01-12 21:11:42 -0800

    first commit

---
+++ make_hdr_graph.py
@@ -0,0 +1,41 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import mplhep as hep
+import numpy as np
+
+labels = ['LHC L1T', 'DUNE', 'IceCube', 'LHCb', 'XENON']
+x = np.array([10e-6, 1e-3, 1, 1000, 60]) # latency [s]
+y = np.array([100e12, 1e12, 20e6, 5e12,  500e6 ]) # throughput [1/s]
+z = np.array([1e6, 50e12, 6e12, 20e15, 50e12]) # buffering [B]
+w = np.array([300e15, 30e15, 300e12, 100e15, 2e15]) # accumulated data volume [B/yr]
+
+plt.style.use([hep.style.ROOT, hep.style.firamath])
+#hep.set_style("CMS")
+#hep.set_style({"font.sans-serif":'Comic Sans MS'})
+
+f, ax = plt.subplots()
+for xi, yi, zi, wi, l in zip(x, y, z, w, labels):
+    ax.plot([xi], [yi], label=l, marker='o', markersize=6*np.log10(wi))
+    if l=='IceCube':
+        ax.text(xi*0.03, yi*30, l)
+    else:
+        ax.text(xi*0.2, yi*30, l)
+#plt.legend()
+ax.plot([1e-2], [1e18], label='1 TB/yr', marker='o', markersize=6*np.log10(1e12), color='gray')
+ax.plot([2e0], [1e18], label='1 PB/yr', marker='o', markersize=6*np.log10(1e15), color='gray')
+ax.plot([1e3], [1e18], label='1 EB/yr', marker='o', markersize=6*np.log10(1e18), color='gray')
+ax.text(0.23e-2, 0.8e18, '1 TB/yr', color='white',size=18)
+ax.text(0.40e0, 0.8e18, '1 PB/yr', color='white',size=18)
+ax.text(0.23e3, 0.8e18, '1 EB/yr', color='white',size=18)
+
+
+ax.loglog()
+ax.set_xlim(1e-7,1e5)
+ax.set_ylim(1e5,1e20)
+ax.set_xlabel('Latency requirement [s]')
+ax.set_ylabel('Streaming data rate [B/s]')
+#hep.cms.label(loc=0)
+
+plt.savefig('hdr_graph.pdf')
+plt.savefig('hdr_graph.png')
