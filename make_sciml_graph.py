6aab0cd8b09377a87b191b29f6378e3191eabf4b
Javier Duarte <jduarte@ucsd.edu>
2022-01-17 16:30:19 -0800

    rename

--- make_sciml_graph.py
+++ make_sciml_graph.py
@@ -8,9 +8,9 @@ import math
 
 # label: data rate lower bound [B/s], data rate upper bound [B/s], latency lower bound [s], latency upper bound [s]
 input_dict = {
-    "LHC L1T": [32 * 40e6, 32 * 40e6, 100e-9, 10e-6],
-    "LHC on Det.": [48 * 40e6, 48 * 40e6, 10e-9, 100e-9],
-    "Accel. Control": [3e3 * 15, 3e3 * 15, 100e-6, 5e-3],
+    "FastML (Jet Class.)": [32 * 40e6, 32 * 40e6, 100e-9, 10e-6],
+    "FastML (Sensor Data Comp.)": [48 * 40e6, 48 * 40e6, 10e-9, 100e-9],
+    "FastML (Beam Control)": [3e3 * 15, 3e3 * 15, 100e-6, 5e-3],
     "MLPerf Tiny (IC)": [3e3 / 100e-3, 3e3 / 1e-3, 1e-3, 100e-3],
     "MLPerf Mobile (NLP)": [1e3 / 100e-3, 1e3 / 40e-3, 40e-3, 100e-3],
 }
@@ -52,9 +52,9 @@ for xloi, xhii, yloi, yhii, l, c in zip(xlo, xhi, ylo, yhi, labels, colors):
     )
     if "Mobile" in l:
         ax.text(xi / 10, yi / 4, l, color=c)
-    elif "Accel" in l:
-        ax.text(xi / 1e5, yi / 1.3, l, color=c)
-    elif "L1T" in l:
+    elif "Beam" in l:
+        ax.text(xi / 4e5, yi * 2, l, color=c)
+    elif "Jet" in l:
         ax.text(xi * 10, yi / 4, l, color=c)
     else:
         ax.text(xi * 2, yi * 2, l, color=c)
